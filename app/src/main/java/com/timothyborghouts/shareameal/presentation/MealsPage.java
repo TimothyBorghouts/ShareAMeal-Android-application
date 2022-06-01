def828a575d11b179c5aa3a47039384ffd2e6ab0
Timothy Borghouts <Timothy.borghouts2@gmail.com>
2022-06-01 08:01:36 +0200

    Created page with meals
    The main activity has been changed to the Meals page and changes to the label and layout have been made.

--- app/src/main/java/com/timothyborghouts/shareameal/presentation/MealsPage.java
+++ app/src/main/java/com/timothyborghouts/shareameal/presentation/MealsPage.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.timothyborghouts.shareameal.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MealsPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.meals_page);
     }
 }
