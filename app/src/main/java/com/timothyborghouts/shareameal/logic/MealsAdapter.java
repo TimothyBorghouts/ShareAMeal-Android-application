b21317ab0b1879563a39e6b475f564c12be4f264
Timothy Borghouts <Timothy.borghouts2@gmail.com>
2022-06-01 22:23:31 +0200

    Created recyclerview with adapter
    Created recyclerview in MealsPage and created recyclerview item + adapter

--- app/src/main/java/com/timothyborghouts/shareameal/logic/MealsAdapter.java
+++ app/src/main/java/com/timothyborghouts/shareameal/logic/MealsAdapter.java
@@ -1,2 +1,68 @@
-package com.timothyborghouts.shareameal.logic;public class MealsAdapter {
+package com.timothyborghouts.shareameal.logic;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.timothyborghouts.shareameal.R;
+import com.timothyborghouts.shareameal.domain.Meal;
+
+import java.util.ArrayList;
+
+public class MealsAdapter extends RecyclerView.Adapter<MealsAdapter.MealViewHolder> {
+
+    private ArrayList<Meal> meals;
+    private Context context;
+    private LayoutInflater mInflater;
+
+    public MealsAdapter(ArrayList<Meal> meals, Context context) {
+        this.meals = meals;
+        this.context = context;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public MealsAdapter.MealViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.meal_recyclerview_item, parent, false);
+        return new MealViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MealsAdapter.MealViewHolder holder, int position) {
+        Meal meal = this.meals.get(position);
+
+        holder.title.setText(meal.getName());
+        holder.date.setText(meal.getDateTime());
+        holder.price.setText(meal.getPrice());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return meals.size();
+    }
+
+    class MealViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        public TextView date;
+        public TextView price;
+
+
+        public MealViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.meal_title);
+            date = itemView.findViewById(R.id.meal_date);
+            price = itemView.findViewById(R.id.meal_price);
+
+        }
+
+    }
+
 }
--- app/src/main/java/com/timothyborghouts/shareameal/presentation/MealsPage.java
+++ app/src/main/java/com/timothyborghouts/shareameal/presentation/MealsPage.java
@@ -1,16 +1,44 @@
 package com.timothyborghouts.shareameal.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.timothyborghouts.shareameal.R;
+import com.timothyborghouts.shareameal.domain.Meal;
+import com.timothyborghouts.shareameal.logic.MealsAdapter;
+
+import java.util.ArrayList;
 
 public class MealsPage extends AppCompatActivity {
+
+    ArrayList<Meal> meals;
+    RecyclerView mealsRecyclerView;
+    MealsAdapter mealsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.meals_page);
+
+        this.meals = fillListWithMeals();
+
+        mealsRecyclerView = findViewById(R.id.meal_recyclerview);
+        mealsAdapter = new MealsAdapter(meals, this);
+
+        mealsRecyclerView.setAdapter(mealsAdapter);
+        mealsRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+
+
     }
+
+    private ArrayList<Meal> fillListWithMeals() {
+        ArrayList<Meal> localMeals = new ArrayList<>();
+        localMeals.add(new Meal("Frietjes", "Friet is geweldig ofzo", true, true, false, true, "2022/01/101", 4, "$4.69", "true", new String[]{"array", "of", "String",}));
+        return localMeals;
+    }
+
 }
